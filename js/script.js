d181fb0fc3e764d58b8a0c967f9682195cbb1aa3
mazy <mazy02@naver.com>
2022-02-14 02:45:31 +0900

    20220214 slide upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// PAGESCROLL
 function pageScroll(){
     let wheelDelta=0;
     let browser=0;
@@ -27,3 +28,32 @@ function pageScroll(){
 };
 pageScroll();
 // console.log($(window).height());
+
+// POPUP SLIDE
+const slide=$('#waterSlide li');
+let current=0;
+setInterval(function(){
+    let prev=slide.eq(current);
+    move(prev, 0, '-100%');
+    current++;
+    if(current==slide.size()) current=0;
+    let next=slide.eq(current);
+    move(next, '100%', 0);
+}, 5000)
+function move(target, start, end){
+    target.css('left', start).animate({left: end}, 500);
+}
+
+/* let page1=0, page2=1,page3=2, timer;
+timer=setInterval(slideEvent, 2500);
+function slideEvent(){
+    slide.eq(page1).stop().css('z-index', 0).animate({left: '100%'}, 1000);
+    slide.eq(page2).stop().css('z-index', 0).animate({left: 0}, 1000);
+    // slide.eq(page3).stop().css('z-index', 1).animate({left: 0}, 1000);
+    page1--;
+    page2--;
+    page3--;
+    if(page1<0) page1=2
+    if(page2<0) page2=2
+    if(page3<0) page3=2
+} */
